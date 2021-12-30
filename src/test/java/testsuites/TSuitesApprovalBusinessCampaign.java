0741ca92943b797f7aa695f0f2f735f093522c62
arbiminanda <arbiminanda10@gmail.com>
2021-12-31 06:10:35 +0700

    slack cucumber testng annotation

--- src/test/java/testsuites/TSuitesApprovalBusinessCampaign.java
+++ src/test/java/testsuites/TSuitesApprovalBusinessCampaign.java
@@ -1,23 +1,49 @@
 package testsuites;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
+import io.cucumber.testng.TestNGCucumberRunner;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
+import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.JUnitCore;
-import org.junit.runner.RunWith;
-import stepsDef.approvalBusinessCampaign;
+import org.testng.annotations.*;
 import utils.Litsteners;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
-                features = "src\\test\\java\\features\\approvalBusinessCampaign.feature",
-                glue ={"stepsDef"},
-                plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-                tags = {"@All"})
+            features = "src/test/java/features/approvalBusinessCampaign.feature",
+            glue = {"stepsDef"},
+            tags = "@All"
+//            plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+)
 
 public class TSuitesApprovalBusinessCampaign {
+
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpCucumber() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void scenario(PickleWrapper pickle, FeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runScenario(pickle.getPickle());
+    }
+
+    @DataProvider
+    public Object[][] scenarios() {
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterMethod
     public static void main(String args[]) {
         JUnitCore runner = new JUnitCore();
         runner.addListener(new Litsteners());
-        runner.run(approvalBusinessCampaign.class);
     }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
+
 }
